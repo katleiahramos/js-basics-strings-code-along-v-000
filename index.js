0e6993a2e4ac63d93c5850fb3b2e9c3707025b2e
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-09 19:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const currentUser = "Katleiah";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
 
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+
+const shortGreeting = `Welcome, `
