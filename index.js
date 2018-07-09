2fc97f6943f0368a47ca564e88b2b2b259ab2a0c
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-09 19:42:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
 
 
-const shortGreeting = `Welcome, `
+const shortGreeting = `Welcome, ${currentUser.charAt(0)} !`
